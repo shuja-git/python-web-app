4c190d89e80348a839c995548602455442880c35
Utkarsh <thaokarutkarsh@gmail.com>
2025-03-06 12:18:51 +0530

    made changes

--- examples/python-web-app/devops/demo/urls.py
+++ examples/python-web-app/devops/demo/urls.py
@@ -5,3 +5,17 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
 ]
+
+# from django.contrib import admin
+# from django.urls import path, include
+# from django.http import HttpResponse
+
+# # Create a simple home page response
+# def home(request):
+#     return HttpResponse("Welcome to the homepage!")
+
+# urlpatterns = [
+#     path('', home),  # Add this line to handle requests to "/"
+#     path('demo/', include('demo.urls')),  # Existing route
+#     path('admin/', admin.site.urls),  # Admin panel
+# ]
